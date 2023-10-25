f4c3a6343cc0e79d2bfccdb4e3fa9b8713b6c8a2
Gustavo Alexadre Silva <alexandregustavo00@gmail.com>
2023-10-25 10:22:41 -0300

    Adicionar arquivos de projeto.

---
+++ CRIPTO_RSA/Gerar_chave.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CRIPTO_RSA
+{
+
+    public class Gerar_Chave
+    {
+     public ulong Mdc(ulong a, ulong b)
+        {
+            while (b!=0)
+            {
+                ulong temp = a % b;
+                a = b;
+                b = temp;
+            }
+            return a;
+        }
+      public bool Checkprimo(ulong n)
+        {
+            if (n < 2) return false;
+            ulong maiordiv = (ulong)Math.Sqrt(n);
+            for (ulong i = 2; i <= maiordiv; i++)
+            {
+                if (n % i == 0) return false;
+            }
+           return true;
+        }
+      public void Genchave(ulong p, ulong q, ulong e)
+        {
+            ulong n = p * q;
+            ulong phi = (p - 1) * (q - 1);
+
+            using (StreamWriter writer = new StreamWriter("chave_publica.txt"))
+            {
+                writer.WriteLine("Chave Pública:");
+                writer.WriteLine("e: " + e);
+                writer.WriteLine("n: " + n);
+            }
+        }
+    }
+}
